578a7899362a01a52e6a11286b4aa31b8e4f98a9
Chicodemilo <mileschick@gmail.com>
2020-01-20 07:03:02 -0600

    Working on drawer nav

--- src/components/Navigation/TruthsNavigator.js
+++ src/components/Navigation/TruthsNavigator.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import {Text} from 'react-native';
 
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createDrawerNavigator} from 'react-navigation-drawer';
 import CategoryDataScreen from '../../screens/CategoryDataScreen';
 import TruthDetailsScreen from '../../screens/TruthDetailsScreen';
+import SearchScreen from '../../screens/SearchScreen';
+import AccountScreen from '../../screens/AccountScreen';
 import Colors from '../../constants/Colors';
 // import {HeaderButtons, Item} from 'react-navigation-header-buttons';
-
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+const defaultStackNavOptions = navData => {
+  return {
+    headerTitleStyle: {
+      fontFamily: 'Ubuntu-Light',
+      fontSize: 14,
+      letterSpacing: 1.3,
+    },
+    headerStyle: {
+      backgroundColor: 'white',
+    },
+    headerTintColor: Colors.primaryRed,
+    headerRight: () => (
+      <Icon
+        name="amazon"
+        style={{color: Colors.primaryRed, fontSize: 23, paddingRight: 15}}
+        onPress={() => {
+          //   navData.navigation.toggleDrawer();
+          console.log('drawer menu');
+        }}
+      />
+      // <HeaderButtons>
+      //   <Item title="HAMS" iconName="menu" />
+      // </HeaderButtons>
+    ),
+  };
+};
 
 const TruthNavigator = createStackNavigator(
   {
@@ -17,31 +46,32 @@ const TruthNavigator = createStackNavigator(
     TruthDetails: TruthDetailsScreen,
   },
   {
-    defaultNavigationOptions: {
-      headerTitleStyle: {
-        fontFamily: 'Ubuntu-Light',
-        fontSize: 14,
-        letterSpacing: 1.3,
-      },
-      headerStyle: {
-        backgroundColor: 'white',
-      },
-      headerTintColor: Colors.primaryRed,
-      headerRight: () => (
-        <Icon
-          name="bars"
-          style={{color: Colors.primaryBlue, fontSize: 23, paddingRight: 15}}
-          onPress={() => {
-            console.log('MENU PRESSED');
-          }}
-        />
-        // <HeaderButtons>
-        //   <Item title="HAMS" iconName="menu" />
-        // </HeaderButtons>
-      ),
-    },
+    defaultNavigationOptions: defaultStackNavOptions,
   },
 );
+
+const SearchNavigator = createStackNavigator(
+  {
+    Search: SearchScreen,
+    Details: TruthDetailsScreen,
+  },
+  {
+    defaultNavigationOptions: defaultStackNavOptions,
+  },
+);
+
+const AccountNavigator = createStackNavigator(
+  {
+    Account: AccountScreen,
+  },
+  {
+    defaultNavigationOptions: defaultStackNavOptions,
+  },
+);
+
+AccountNavigator.navigationOptions = {
+  headerTitle: 'Your Account',
+};
 
 //can do as an object on each navigation screen:
 // const TruthNavigator = createStackNavigator({Truths: {
@@ -60,4 +90,37 @@ const TruthNavigator = createStackNavigator(
 //     }
 // });
 
-export default createAppContainer(TruthNavigator);
+const TruthTabNavigator = createBottomTabNavigator(
+  {
+    MyTruths: {
+      screen: TruthNavigator,
+      navigationOptions: {
+        tabBarIcon: tabInfo => {
+          return <Icon name="anchor" size={25} color={tabInfo.tintColor} />;
+        },
+      },
+    },
+    Search: {
+      screen: SearchNavigator,
+      navigationOptions: {
+        tabBarLabel: 'Enter The Dragon',
+        tabBarIcon: tabInfo => {
+          return <Icon name="search" size={25} color={tabInfo.tintColor} />;
+        },
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.primaryRed,
+    },
+  },
+);
+
+const MainNavigator = createDrawerNavigator({
+  MyTruths: TruthTabNavigator,
+  Search: SearchNavigator,
+  MyAccount: AccountNavigator,
+});
+
+export default createAppContainer(MainNavigator);
--- src/screens/CategoryDataScreen.js
+++ src/screens/CategoryDataScreen.js
@@ -3,6 +3,8 @@ import {View, Text, TouchableOpacity, FlatList} from 'react-native';
 import styles from '../../styles/mainStyles';
 import {TRUTHS} from '../data/dummy-data';
 import TruthsGridTile from '../components/Navigation/TruthsGridTile';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Colors from '../constants/Colors';
 
 const CategoryDataScreen = props => {
   const renderGridItem = itemData => {
@@ -23,9 +25,24 @@ const CategoryDataScreen = props => {
   };
   return <FlatList data={TRUTHS} renderItem={renderGridItem} numColumns={2} />;
 };
-
-CategoryDataScreen.navigationOptions = {
-  headerTitle: 'All Truths',
+CategoryDataScreen.navigationOptions = navData => {
+  return {
+    headerRight: () => (
+      <Icon
+        name="bars"
+        style={{color: Colors.primaryBlue, fontSize: 23, paddingRight: 15}}
+        onPress={() => {
+          navData.navigation.toggleDrawer;
+        }}
+      />
+      // <HeaderButtons>
+      //   <Item title="HAMS" iconName="menu" />
+      // </HeaderButtons>
+    ),
+  };
 };
+// CategoryDataScreen.navigationOptions = {
+//   headerTitle: 'All Truths',
+// };
 
 export default CategoryDataScreen;
